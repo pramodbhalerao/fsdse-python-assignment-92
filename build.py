333b13a848e11bfedc80bc9a99af54284f7fd481
pramodbhalerao <pramodbhalerao_01@yahoo.com>
2017-06-24 07:06:03 +0000

    Done

--- build.py
+++ build.py
@@ -1,8 +1,18 @@
 import pandas as pd
-import numpy as np
+import numpy
 
 
 def solution(data, index):
-    """
-    Enter your code here
-    """
+    data_with_updated_labels = pd.DataFrame(data, index=index)
+    return data_with_updated_labels
+
+exam_data = {
+            'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+            'score': [12.5, 9, 16.5, numpy.nan, 9, 20, 14.5, numpy.nan, 8, 19],
+            'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+            'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']
+        }
+
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+solution(exam_data,labels)
